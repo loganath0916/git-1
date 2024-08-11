b79355886c0e0635e22c29bc114fcd0573e262b0
loganath0916 <loganath0916@gmail.com>
2024-08-11 18:43:33 +0530

    modifed

--- explore.java
+++ explore.java
@@ -1,38 +1,18 @@
-class student
-{
-    int rollno ;
-    String name ;
-    int mark ;
-}
-public class explore {
-    public static void main(String a[])
-    {
-       student s1 = new student();
-        s1.rollno = 1;
-        s1.name = "logu";
-        s1.mark =50;
+ import java.util.Scanner;
 
-        student s2 = new student();
-        s2.rollno = 2;
-        s2.name = "loves";
-        s2.mark =50;
-
-        student s3 = new student();
-        s3.rollno = 3;
-        s3.name = "varsha";
-        s3.mark =50;
-
-        student stu [] = new student [5];
-        stu [0] = s1;
-        stu [1] = s2;
-        stu [2] = s3;
+ public class explore
+ {
+     public static void main(String [] args)
+     {
+       Scanner sc = new Scanner (System.in );
+       int a = sc.nextInt();
+        
+      for(int b = 2;b%a==0;a++ )
+      {
+        System.out.println("nota prime");
+      }
 
 
-     for ( int i=0; i<stu.length; i++)
-    {
-     System.out.println(stu[i].rollno + "." + stu[i].name + ":" + stu[i].mark ); 
-    }
+     }
+
  }
-
-
-}
